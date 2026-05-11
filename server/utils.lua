ed77f3703a8857ce82d5d32e303d90c3e3d6a509
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-11 14:53:51 +0200

    feat(utils): hook registration wrapper

--- server/utils.lua
+++ server/utils.lua
@@ -15,3 +15,33 @@ function SatisfiesPatterns(value, patterns)
 
     return false
 end
+
+local hookSideEffectListenersRecordSeeLongNamesAreFun = {}
+
+---basic wrapper used for this resource, pitches into DRY coding practices
+---@param hookname 'swapItems' | 'openInventory' | 'openShop' | 'createItem' | 'buyItem' | 'craftItem' | 'usingItem'
+---@param hookcb function | nil
+---@param posthookcb function | nil
+---@param patterns table<string>
+function RegisterHookAction(hookname, hookcb, posthookcb, patterns)
+	local hookid = exports.ox_inventory:registerHook(hookname, hookcb, { inventoryFilter = patterns })
+
+	if not posthookcb then return end
+
+	local listner = AddEventHandler(hookid, function (success, payload)
+		if not success then return end
+
+		posthookcb(payload)
+	end)
+
+	hookSideEffectListenersRecordSeeLongNamesAreFun[#hookSideEffectListenersRecordSeeLongNamesAreFun+1] = listner
+end
+
+AddEventHandler('onResourceStop', function (resource)
+	if resource ~= 'ox_inventory' then return end
+
+	for i = 1, #hookSideEffectListenersRecordSeeLongNamesAreFun, 1 do
+		local listener = hookSideEffectListenersRecordSeeLongNamesAreFun[i]
+		RemoveEventHandler(listener)
+	end
+end)
