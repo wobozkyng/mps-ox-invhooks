cb29d71593546b307622fb9f994227466ee34551
Maximus <the.maximus.7474@gmail.com>
2026-05-13 13:47:30 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ server_only "yes"
 description "ox inventory hooks"
 repository 'https://github.com/Maximus7474/mps-ox-invhooks'
 
-version "1.0.0"
+version "1.1.1"
 
 server_scripts {
 	"@ox_lib/init.lua",
