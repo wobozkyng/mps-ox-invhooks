5af5c98ab38574a32062225cd8ee29aeefb5d2c3
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-22 01:05:54 +0200

    revert: 8832518552802a5389824dc15b71103aae78f648

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ server_only "yes"
 description "ox inventory hooks"
 repository 'https://github.com/Maximus7474/mps-ox-invhooks'
 
-version 'v1.2.0'
+version 'v1.1.1'
 
 server_scripts {
 	"@ox_lib/init.lua",
